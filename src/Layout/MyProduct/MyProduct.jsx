5965d56a1929522e0853c7270742c79a01f0bf94
UR Raju <riadhasanraju>
2024-01-04 03:34:11 +0600

    tonight last push

--- src/Layout/MyProduct/MyProduct.jsx
+++ src/Layout/MyProduct/MyProduct.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../Components/useHooks/useAuth";
 import useAxiosPublic from "../../Components/useHooks/useAxiosPublic";
 import { Link } from "react-router-dom";
+import { data } from "autoprefixer";
 
 const MyProduct = () => {
   const { user } = useAuth();
@@ -17,21 +18,60 @@ const MyProduct = () => {
   return (
     <div className="px-2 md:px-5 my-10">
       <div className="max-w-screen-2xl mx-auto">
-        <div className="flex justify-between">
-          <p>Total Quantity : {userProduct.length}</p>
-          <p>Total Price : {}</p>
+        <div className="flex justify-between px-1 mb-2">
+          <p className="text-lg  border border-teal-100 font-semibold text-teal-500 rounded px-3   py-1   ">
+             Quantity : {userProduct.length}
+          </p>
+          <p className="text-lg border border-cyan-100 font-semibold text-teal-500 rounded px-3   py-1  ">
+            Total Price : {}
+          </p>
         </div>
-        {userProduct.map((product) => (
-          <div key={product._id}>
-            <div className="flex justify-between rounded-lg items-center mb-2 bg-orange-100 p-4 ">
-                <img className="w-20 border-r-2" src={product.photo} alt="" />
-                <p className="text-lg capitalize font-semibold">{product.name}</p>
-                <p className="text-lg capitalize font-semibold">{product.brand}</p>
-                <p className="text-lg capitalize font-semibold">{product.price}</p>
-                <Link to={`/details/${product.productId}`}>See Details</Link>
-            </div>
-          </div>
-        ))}
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr className="bg-orange-200 ">
+              <th className="uppercase font-lexend  font-bold">NO</th>
+              <th className="uppercase font-lexend font-bold">Image</th>
+              <th className="uppercase font-lexend font-bold">Name</th>
+              <th className="uppercase font-lexend font-bold">Brand</th>
+              <th className="uppercase font-lexend font-bold">Price</th>
+              <th className="uppercase font-lexend font-bold">See Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userProduct.map((product, index) => (
+              <tr key={product._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <img className="w-20 rounded-md" src={product.photo} alt="" />
+                </td>
+                <td>
+                  <p className="font-semibold capitalize text-lg">
+                    {product.name}
+                  </p>
+                </td>
+                <td>
+                  <p className="font-semibold text-lg capitalize">
+                    {product.brand}
+                  </p>
+                </td>
+                <td>
+                  <p className="font-semibold text-lg capitalize">
+                    ${product.price}
+                  </p>
+                </td>
+
+                <td>
+                  <Link to={`/details/${product.productId}`}>
+                    <button className="bg-orange-500 rounded text-white px-2 py-1">
+                      Details
+                    </button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- src/Pages/AllProducts.jsx
+++ src/Pages/AllProducts.jsx
@@ -3,27 +3,62 @@ import useAllData from "../Components/useHooks/useAllData";
 import ProductsCard from "./ProductsCard";
 
 const AllProducts = () => {
-    const [products,refetch] = useAllData()
-    console.log(products);
-
-    const [sorting, setSorting] = useState([]);
-
+  const [products, refetch] = useAllData();
+  console.log(products);
+  const [displayCards, setDisplayCards] = useState([...products]);
+  const [search, setSearch] = useState([...products]);
+  
+  const handleSearch = (e) => {
+    let input = e.target.value.toLowerCase();
+    const searchCards = search.filter((card) =>
+      card.name.toLowerCase().startsWith(input)
+    );
+    setDisplayCards(searchCards);
+  };
+  const [sorting, setSorting] = useState([]);
   const handleSorting = (e) => {
     setSorting(e.target.value);
   };
   if (sorting === "high-to-low") {
-    products.sort((a, b) => b.price - a.price);
+    displayCards.sort((a, b) => b.price - a.price);
   } else if (sorting === "low-to-high") {
-     products.sort((a, b) => a.price - b.price);
+    displayCards.sort((a, b) => a.price - b.price);
   }
-    return(
-        <div className="px-2 md:px-5 my-20">
-            <div className="text-center">
-                <h1 className="capitalize font-inter font-bold text-4xl ">Our All <span className="text-orange-500">Products</span></h1>
-                <p className="text-lg tracking-wider capitalize mt-2 text-gray-500">You can chose any products and buy now. Lorem ipsum dolor <br /> sit amet, consectetur adipisicing elit. Fuga, culpa!</p>
-            </div>
 
-            <div className="max-w-7xl mx-auto text-right">
+  return (
+    <div className="px-2 md:px-5 my-20">
+        {/* heading tilte  */}
+      <div className="text-center">
+        <h1 className="capitalize font-inter font-bold text-4xl ">
+          Our All <span className="text-orange-500">Products</span>
+        </h1>
+        <p className="text-lg tracking-wider capitalize mt-2 text-gray-500">
+          You can chose any products and buy now. Lorem ipsum dolor <br /> sit
+          amet, consectetur adipisicing elit. Fuga, culpa!
+        </p>
+      </div>
+
+        {/* search Product function  */}
+      <div className="flex justify-center mt-5">
+        <div className=" py-2  px-3 mt-6 relative  rounded bg-black inline-block">
+          <input
+            onChange={handleSearch}
+            name="search"
+            className="outline-none md:w-80 lg:w-96  text-white bg-transparent"
+            type="text"
+            placeholder="Search here..."
+          />
+          <button
+            type="button"
+            className="bg-orange-500 rounded-r top-0 right-0 absolute  text-white py-2 px-4"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+        {/* filter product function  */}
+      <div className="max-w-7xl mx-auto text-right">
         <select
           className=" bg-orange-500 border-l-4 border-gray-300 outline-none rounded mt-4 shadow-xl  font-normal tracking-wider px-2 py-1 text-white"
           onChange={handleSorting}
@@ -37,10 +72,13 @@ const AllProducts = () => {
           <option value="low-to-high">Low To High</option>
         </select>
       </div>
-           
-           <div className="max-w-screen-2xl mt-10 mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3">
-            {products.map(product => <ProductsCard key={product._id} data={product} refetch={refetch}/>)}
-           </div>
-        </div>
-    )}
+
+      <div className="max-w-screen-2xl mt-10 mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3">
+        {displayCards.map((product) => (
+          <ProductsCard key={product._id} data={product} refetch={refetch} />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default AllProducts;
